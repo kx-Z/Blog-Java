58ceff800b1346ce1d67a728fcf7b910898a71de
kx-Z <84176922+kx-Z@users.noreply.github.com>
2023-10-22 22:19:25 +0800

    Delete 源码/01-Java SE/Study directory

--- 源码/01-Java SE/Study/src/github/oop/Demo04/Teacher.java
+++
@@ -1,9 +0,0 @@
-package github.oop.Demo04;
-
-/**
- * @author subeiLY
- * @create 2021-06-04 12:25
- */
-public class Teacher extends Person{
-
-}
--- 源码/01-Java SE/Study/src/github/oop/Demo06/Teacher.java
+++
@@ -1,9 +0,0 @@
-package github.oop.Demo06;
-
-/**
- * @author subeiLY
- * @create 2021-06-05 4:10
- */
-public class Teacher extends Person{
-
-}
